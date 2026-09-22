aff044aa4bc2678af56b61e6b2d615c02eccec4b
tibidias <baker.616696@swl.k12.oh.us>
2026-09-22 11:21:38 -0400

    Task3.java

--- src/Task3.java
+++ src/Task3.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Task3 {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        double springCost;
+        double summerCost;
+        double fallCost;
+        double winterCost;
+
+        System.out.println("Insert your maintenance cost for the four seasons (spring, summer, fall, winter).");
+        springCost = scanner.nextDouble();
+        summerCost = scanner.nextDouble();
+        fallCost = scanner.nextDouble();
+        winterCost = scanner.nextDouble();
+        System.out.println("Your yearly maintenance cost would be $" + (springCost + summerCost + fallCost + winterCost) + ".");
+    }
 }
