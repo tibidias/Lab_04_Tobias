228f8c422a059dce5f520aeec0fca3be32c82ffd
tibidias <baker.616696@swl.k12.oh.us>
2026-09-22 11:22:00 -0400

    Task2.java

--- src/Task2.java
+++ src/Task2.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
 
 public class Task2 {
+    static void main() {
+        double userPrice;
+        double finalPrice;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is the price of your purchase.");
+        userPrice = scanner.nextDouble();
+        finalPrice = userPrice * 1.05;
+        System.out.println("Your final price with tax is $" + finalPrice + ".");
+    }
 }
