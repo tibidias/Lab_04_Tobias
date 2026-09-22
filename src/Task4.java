a16261f3513ae15d548cd47f6a61c218488a3c9a
tibidias <baker.616696@swl.k12.oh.us>
2026-09-22 11:21:18 -0400

    Task4.java

--- src/Task4.java
+++ src/Task4.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+public class Task4 {
+    static void main() {
+        int creditBalance = 5000;
+        double oneMonthBalance = (double)(creditBalance * 1.17);
+        double twoMonthBalance = (double)(creditBalance * (1.17 * 1.17));
 
-public class task4 {
+        System.out.println("With a balance of $5000 and a 17% interest rate after one month the balance would be $" + oneMonthBalance + " and after two months the balance would be $" + twoMonthBalance + ".");
+    }
 }
