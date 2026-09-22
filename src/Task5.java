322330b2e38b80fc431f681f64e2c1cb9477f4f2
tibidias <baker.616696@swl.k12.oh.us>
2026-09-22 11:20:56 -0400

    Task5.java

--- src/Task5.java
+++ src/Task5.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Task5 {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        double tempF;
+        double tempC;
+
+        System.out.println("Enter your temperature in degrees F.");
+        tempF = scanner.nextInt();
+        tempC = ((tempF - 32) * 5/9);
+
+        System.out.printf("Your temperature in degrees C is " + tempC + " degrees.");
+    }
 }
